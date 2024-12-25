3ed7691cc3dfe880c790318bd95d20ed41d7ff3e
jiangyh <1162242058@qq.com>
2024-12-25 16:53:11 +0800

    初始化目录

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import sys
 from PySide6.QtWidgets import QApplication
 
 from app.view.main_window import MainWindow
-from app.recource import resource_rc
+from app.resource import resource_rc
+
 if __name__ == '__main__':
     os.environ["QT_SCALE_FACTOR"] = "Auto"
     os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
 
     app = QApplication(sys.argv)
+
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
