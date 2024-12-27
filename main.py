4cada567c30c1593a81f3d42866afdeac1ff1e69
jiangyh <1162242058@qq.com>
2024-12-27 09:31:58 +0800

    更新

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PySide6.QtWidgets import QApplication
 from app.view.main_window import MainWindow
 from app.recource import resource_rc
 
+
 if __name__ == '__main__':
     os.environ["QT_SCALE_FACTOR"] = "Auto"
     os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
